0b10a031c86aab6e67f39edc6f33845d0e52d5b2
Erwin Amador <eamador0305@gmail.com>
2023-09-26 12:36:46 -0600

    Create multiple pages and router try out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Error from './pages/Error'
+import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
+  let activeClassName = "nav-active";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <h1>Hello World</h1>
       </header>
-    </div>
+      <nav>
+      <NavLink to="" className={({ isActive }) => isActive && activeClassName}>Home</NavLink>
+      <NavLink to="about" className={({ isActive }) => isActive && activeClassName}>About</NavLink>
+      <NavLink to="contact" className={({ isActive }) => isActive && activeClassName}>Contact</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="contact" element={<Contact/>} />
+        <Route path="*" element={<Error/>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
